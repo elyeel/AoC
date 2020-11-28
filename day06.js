5f9e7ab7b15209e3ab5277a500b87c4592e31df2
Risyardi Iswanto <elyeel@yahoo.com>
2020-11-27 17:22:01 -0800

    day 06 solution to learn

--- day06.js
+++ day06.js
@@ -1,22 +1,65 @@
 const fs = require("fs");
-const input = fs.readFileSync("./texts/example06.txt", "utf8").split("\n");
+const input = fs.readFileSync("./texts/text06.txt", "utf8").split("\n");
 const orbital = [];
 const tree = {};
 
-const plot = input.map(node => {
-  // Plot the orbital orbits
-  orbital.push(node.split(")"));
-});
-
-const plotted = orbital.map(x => {
-  let orb = tree[x[0]];
-  if (orb == undefined) {
-    tree[x[0]] = x[1];
-  } else {
-    tree[x[0]] = [];
-    tree[x[0]].push(x[1]);
-    tree[x[0]].push(orb);
+const result = input.map((elem) => {
+  if (!tree[elem[0]]) {
+    tree[elem[0]] = [];
   }
+  tree[elem[0]].push(elem[1]);
 });
 
-console.log(orbital.length, orbital, tree);
+for (const [planet, orbiter] of Object.entries(tree)) {
+  orbiter.map((x) => {
+    tree;
+  });
+}
+
+const findOrbiter = (input, tag) => {
+  return input.filter((planet) => planet[0] === tag);
+};
+
+// tree.COM = findOrbiter(input, "B");
+// console.log(result, tree);
+
+// solution from web
+const findElement = (input, tag) => input.filter((x) => x.split(")")[1] == tag);
+
+const getOrbits = (input, element, orbits = 1) => {
+  if (!getOrbits.checkedOrbits) getOrbits.checkedOrbits = {};
+
+  const [centre, object] = element.split(")");
+  if (centre == "COM") {
+    getOrbits.checkedOrbits[object] = 1;
+    return orbits;
+  }
+  if (getOrbits.checkedOrbits.hasOwnProperty(centre)) {
+    getOrbits.checkedOrbits[object] = getOrbits.checkedOrbits[centre] + 1;
+    return orbits + getOrbits.checkedOrbits[centre];
+  }
+  const [nextObject] = findElement(input, centre);
+  return getOrbits(input, nextObject, orbits + 1);
+};
+
+const getOrbitChain = (input, element, chain = []) => {
+  const [centre, object] = element.split(")");
+  const newChain = chain.concat(centre);
+  if (centre == "COM") return newChain;
+  const [nextObject] = findElement(input, centre);
+  return getOrbitChain(input, nextObject, newChain);
+};
+
+const calculateTransfers = (input, origin, destination) => {
+  const fromYou = getOrbitChain(input, origin);
+  const fromDest = getOrbitChain(input, destination);
+  const commonObject = fromYou.filter((x) => fromDest.includes(x))[0];
+  return fromYou.indexOf(commonObject) + fromDest.indexOf(commonObject) - 2;
+};
+
+const data = input;
+const partOne = data.reduce((a, b) => a + getOrbits(data, b), 0);
+const partTwo = calculateTransfers(data, "YOU", "SAN");
+
+console.log(partOne, partTwo);
+// console.log(orbital.length, orbital, tree, plotted);
