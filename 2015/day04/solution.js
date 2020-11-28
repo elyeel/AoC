573caf44a87f6b430b4c70cda13e70ca93ef4e41
Risyardi Iswanto <elyeel@yahoo.com>
2020-11-27 22:14:09 -0800

    day04 solved

--- 2015/day04/solution.js
+++ 2015/day04/solution.js
@@ -7,8 +7,11 @@ const INPUT = input;
 
 const md5 = (data) => crypto.createHash("md5").update(data).digest("hex");
 const isStartsWithFiveZeros = (data) => data.slice(0, 5) === "00000";
+const isStartsWithSixZeros = (data) => data.slice(0, 6) === "000000";
 
-let counter = 0;
+let counter = 0, counter6 = 0;
 while (!isStartsWithFiveZeros(md5(`${INPUT}${counter}`))) counter++;
+while (!isStartsWithSixZeros(md5(`${INPUT}${counter6}`))) counter6++;
 
-console.log(counter);
+console.log(counter, counter6);
+
