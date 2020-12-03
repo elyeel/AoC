598cd35a0b2ac61e92e9e74efd5b6bbc4bba5573
Risyardi Iswanto <elyeel@yahoo.com>
2020-12-02 22:08:50 -0800

    solving day03 2020

--- 2015/day07/solution.js
+++ 2015/day07/solution.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("./2015/day07/control.txt", "utf8").split("\n");
+const input = fs.readFileSync("./2015/day07/data.txt", "utf8").split("\n");
 console.log(input.length);
 
 const result = {};
@@ -11,13 +11,14 @@ const bitwise = {
   LSHIFT: (a, b) => a << b,
   RSHIFT: (a, b) => a >> b,
 };
-const bitRegex = /([A-Z])\w+/g;
-const commRegex = /[a-z0-9]+/g;
+const commRegex = /([A-Z])+/g;
+const bitRegex = /[a-z0-9]+/g;
 
 const parseInstruction = (instruction) => {
   const command = instruction.match(commRegex);
   const args = instruction.match(bitRegex);
   const destination = args.pop();
+  // console.log(instruction, command, args, destination);
 
   return {
     command: command,
@@ -26,4 +27,57 @@ const parseInstruction = (instruction) => {
   };
 };
 
-input.map((line) => {});
+const filteredInput = input.filter((line) => {
+  const instruct = parseInstruction(line);
+  // console.log(instruct);
+
+  if (!instruct.command) return line;
+});
+
+const filteredInput2 = input.filter((line) => {
+  const instruct = parseInstruction(line);
+  if (["a", "b", "c"].some((x) => x === instruct.args.map((y) => y))) return line;
+});
+
+const calculateOp = (instruct) => {
+  // base case for recursive
+  // if ()
+
+  if (instruct.command) {
+    if (instruct.command[0] === "LSHIFT" || instruct.command[0] === "RSHIFT") {
+      result[instruct.destination] = bitwise[instruct.command[0]](
+        result[instruct.args[0]],
+        instruct.args[1]
+      );
+    }
+    if (instruct.command[0] === "AND" || instruct.command[0] === "OR") {
+      result[instruct.destination] = bitwise[instruct.command[0]](
+        result[instruct.args[0]],
+        result[instruct.args[1]] ? result[instruct.args[1]] : null
+      );
+    }
+    if (instruct.command[0] === "NOT") {
+      // console.log(result[instruct.args[0]]);
+      result[instruct.destination] =
+        bitwise[instruct.command[0]](result[instruct.args[0]]) + 65536;
+    }
+  } else {
+    result[instruct.destination] = instruct.args[0];
+  }
+};
+
+const test = (data) => {
+  data.map((line, i) => {
+    const instruct = parseInstruction(line);
+    if (result[instruct.destination]) {
+      // destination exist in result go here
+    } else {
+      // destination not exist in result here, create key: value
+      //creating new key to result
+      calculateOp(instruct);
+    }
+  });
+};
+// test();
+console.log(filteredInput, filteredInput2);
+// console.log(result);
---
+++ 2020/day03/solution.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const input = fs.readFileSync("./2020/day03/data.txt", "utf8").split("\n");
+console.log(input.length);
+
+let count = 0,
+  count2 = 0;
+let position = 3;
+const slopes = [1, 3, 5, 7, "12"];
+
+const findTree = (input) => {
+  for (let i = 1; i < input.length; i++) {
+    // position > 30 ? (position = 0) : position;
+    if (position > 30) {
+      position -= 31;
+    }
+    // console.log(input[i].split("")[position]);
+    input[i].split("")[position] === "#" ? count++ : count;
+    position += 3;
+  }
+  return count;
+};
+
+console.log(findTree(input));
+
+const findTree2 = (input, pos) => {
+  let count2 = 0;
+  let position = pos;
+  for (let i = 1; i < input.length; i++) {
+    // position > 30 ? (position = 0) : position;
+    if (position > 30) {
+      position -= 31;
+    }
+    // console.log(input[i].split("")[position]);
+    input[i].split("")[position] === "#" ? count2++ : count2;
+    position += pos;
+  }
+  return count2;
+};
+
+const findTree3 = (input, pos) => {
+  let count3 = 0;
+  let position = pos;
+  for (let i = 2; i < input.length; i += 2) {
+    // position > 30 ? (position = 0) : position;
+    if (position > 30) {
+      position -= 31;
+    }
+    // console.log(input[i].split("")[position]);
+    input[i].split("")[position] === "#" ? count2++ : count2;
+    position += pos;
+  }
+  return count3;
+};
+
+console.log(findTree2(input, 1));
+console.log(findTree2(input, 3));
+console.log(findTree2(input, 5));
+console.log(findTree2(input, 7));
+console.log(findTree3(input, 1));
