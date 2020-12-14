f074856630e05f78d8f2fb59dfbd41ce7c16edf9
Risyardi Iswanto <elyeel@yahoo.com>
2020-12-14 13:19:59 -0800

    solved day14 2020

--- 2020/day14/solution.js
+++ 2020/day14/solution.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("./2020/day14/control2.txt", "utf8").split("\n");
+const input = fs.readFileSync("./2020/day14/data.txt", "utf8").split("\n");
 // .map((x) => parseInt(x));
 console.log(input.length);
 
@@ -12,7 +12,7 @@ function decToBin(dec) {
   return result;
 }
 
-// console.log(decToBin(101).length);
+// console.log(decToBin(101));
 
 const applyMask = (mask, dec) => {
   let result = "";
@@ -61,36 +61,68 @@ for (const entry of Object.values(resultObj)) {
 console.log(sum);
 
 // Part 2
+const genBinaryCombo = (numX) => {
+  const replaceXWith = [];
+  for (let j = 0; j < 2 ** numX; j++) {
+    replaceXWith.push((j >>> 0).toString(2).padStart(numX, 0));
+  }
+  return replaceXWith;
+};
+// console.log(genBinaryCombo(3));
+
 const applyMask2 = (mask, bin) => {
   let result = [];
 
   const numX = mask.split("").reduce((a, c) => (c === "X" ? a + 1 : a + 0), 0);
-  for (let x = 0; x < numX; ++x) {
-    let res1 = "",
-      res2 = "";
+  const binComboArr = genBinaryCombo(numX);
+
+  for (let x = 0; x < 2 ** numX; ++x) {
+    let res = "";
+    let xNo = 0;
+
     for (let i = 0; i < mask.length; ++i) {
       if (mask[i] === "0") {
-        res1 += bin[i];
-        res2 += bin[i];
+        res += bin[i];
       }
       if (mask[i] === "1") {
-        res1 += "1";
-        res2 += "1";
+        res += "1";
       }
       if (mask[i] === "X") {
-        if (x % 2 === 0) {
-          res1 += "1";
-          res2 += "0";
-        } else {
-          res1 += "0";
-          res2 += "1";
-        }
+        res += binComboArr[x][xNo];
+        xNo++;
       }
     }
-    result.push(res1);
-    result.push(res2);
+    result.push(res);
   }
   return result;
 };
 
-console.log(applyMask2("000000000000000000000000000000X1001X", decToBin(42)));
+// console.log(applyMask2("00000000000000000000000000000000X0XX", decToBin(26)));
+
+const part2Runner = (lines) => {
+  const resultMap = new Map();
+  let currMask;
+
+  for (const line of lines) {
+    const [maskMem, value] = line.split(" = ");
+    if (maskMem === "mask") {
+      currMask = value;
+      continue;
+    }
+
+    const addr = maskMem.match(/(\d+)/)[1];
+    const bin = decToBin(addr);
+    const memAddress = applyMask2(currMask, bin);
+    memAddress.forEach((mem) => {
+      const address = binToDec(mem);
+      resultMap.set(parseInt(address), parseInt(value));
+    });
+  }
+
+  return resultMap;
+};
+
+const memory = part2Runner(input);
+let memorySum = 0;
+memory.forEach((x) => (memorySum += x));
+console.log(memorySum);
