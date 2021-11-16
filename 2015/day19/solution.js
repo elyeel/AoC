71267c86a9e0d18cd57891bea74b8c4ad4940074
Risyardi Iswanto <elyeel@yahoo.com>
2021-11-15 19:24:45 -0800

    moved 2019 to a folder (check file locations), solving day10 2019

--- 2015/day19/solution.js
+++ 2015/day19/solution.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-let input = fs.readFileSync("./2015/day19/data.txt", "utf8"); //.split("\n");
+import fs from 'fs';
+let input = fs.readFileSync('./2015/day19/data.txt', 'utf8'); //.split("\n");
 // .map((x) => parseInt(x));
 // console.log(input.length);
 // const regex = /[A-Z]{1}[a-z]?/g;
@@ -23,55 +23,55 @@ let input = fs.readFileSync("./2015/day19/data.txt", "utf8"); //.split("\n");
 // console.log(combinationObj.length);
 
 function test() {
-  input = input.split("").reverse().join("").split("\n");
+	input = input.split('').reverse().join('').split('\n');
 
-  var molecule = input.shift();
-  input.shift(); // empty line
+	var molecule = input.shift();
+	input.shift(); // empty line
 
-  input = input.map(function (a) {
-    return a.split(" >= ");
-  });
+	input = input.map(function(a) {
+		return a.split(' >= ');
+	});
 
-  var uniqueNewMolecules = {};
-  var i, y;
+	var uniqueNewMolecules = {};
+	var i, y;
 
-  for (i = 0; i < molecule.length; i++) {
-    for (y = 0; y < input.length; y++) {
-      if (molecule.substr(i, input[y][1].length) === input[y][1]) {
-        uniqueNewMolecules[
-          molecule.slice(0, i) +
-            input[y][0] +
-            molecule.slice(i + input[y][1].length)
-        ] = true;
-      }
-    }
-  }
+	for (i = 0; i < molecule.length; i++) {
+		for (y = 0; y < input.length; y++) {
+			if (molecule.substr(i, input[y][1].length) === input[y][1]) {
+				uniqueNewMolecules[
+					molecule.slice(0, i) +
+						input[y][0] +
+						molecule.slice(i + input[y][1].length)
+				] = true;
+			}
+		}
+	}
 
-  var steps = 0;
-  var lookup = {};
-  var replaced = false;
+	var steps = 0;
+	var lookup = {};
+	var replaced = false;
 
-  input = input.map(function (a) {
-    lookup[a[0]] = a[1];
+	input = input.map(function(a) {
+		lookup[a[0]] = a[1];
 
-    return a[0];
-  });
+		return a[0];
+	});
 
-  var regexp = new RegExp("(" + input.join("|") + ")", "g");
-  var replaceCallback = function (matched) {
-    replaced = true;
+	var regexp = new RegExp('(' + input.join('|') + ')', 'g');
+	var replaceCallback = function(matched) {
+		replaced = true;
 
-    steps++;
+		steps++;
 
-    return lookup[matched];
-  };
+		return lookup[matched];
+	};
 
-  do {
-    replaced = false;
-    molecule = molecule.replace(regexp, replaceCallback);
-  } while (replaced);
+	do {
+		replaced = false;
+		molecule = molecule.replace(regexp, replaceCallback);
+	} while (replaced);
 
-  return [Object.keys(uniqueNewMolecules).length, steps];
+	return [ Object.keys(uniqueNewMolecules).length, steps ];
 }
 
 console.log(test());
