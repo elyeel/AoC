70f4c67d3d45eeeb3661181171447c30a9a1a503
Risyardi Iswanto <elyeel@yahoo.com>
2021-11-16 16:35:26 -0800

    solved part1 day10 2019

--- 2019/day10/solution.js
+++ 2019/day10/solution.js
@@ -1,15 +1,15 @@
 import fs from 'fs';
 
-const data = fs.readFileSync('./2019/day10/control2.txt', 'utf8').split('\n');
+const data = fs.readFileSync('./2019/day10/data.txt', 'utf8').split('\n');
 console.log(data);
 
 const coordMap = new Map();
 data.forEach((line, y) => {
-	line
-		.split('')
-		.forEach((dot, x) =>
-			coordMap.set(`${x} ${y}`, { x, y, value: dot, detected: 0 })
-		);
+	line.split('').forEach((dot, x) => {
+		// const a = x + 1;
+		// const b = y + 1;
+		coordMap.set(`${x} ${y}`, { x, y, value: dot, detected: 0 });
+	});
 });
 
 //outside, looking for '#', then using the current coordinate to find x/y in distance from current x,y
@@ -19,17 +19,31 @@ for (let [ coord, valueObj ] of coordMap.entries()) {
 		const asteroid = new Set();
 		coordMap.forEach((valObj) => {
 			if (valObj.value === '#') {
-				if (valueObj.x === valObj.x && valueObj.y === valObj.y) {
+				// will use degree
+				if (valueObj.x !== valObj.x || valueObj.y !== valObj.y) {
+					// console.log(coord);
+
+					const dx = valObj.x - valueObj.x;
+					const dy = valObj.y - valueObj.y;
+					const arcTan = Math.atan2(dy, dx);
+					asteroid.add(arcTan);
 				} else {
-					const x = valObj.x - valueObj.x;
-					const y = valObj.y - valueObj.y;
-					asteroid.add(x / y);
-					console.log(valueObj.x, '-', valueObj.y, '=>', `${x}/${y}`);
 				}
 			}
 		});
+		// console.log(asteroid);
 		valueObj.detected += asteroid.size;
 	}
 }
 
-console.log(coordMap.size, coordMap);
+// console.log(coordMap.size, coordMap);
+let max = { x: 0, y: 0, value: 0 };
+for (const obj of coordMap.values()) {
+	if (obj.detected > max.value) {
+		max.x = obj.x;
+		max.y = obj.y;
+		max.value = obj.detected;
+	}
+}
+console.log(max);
+//part 2
