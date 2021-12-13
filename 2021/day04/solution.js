78ad03fd5b6f46f5e9fe7b60c08a241209e49ae5
Risyardi Iswanto <elyeel@yahoo.com>
2021-12-13 13:27:14 -0800

    solved 2021 day04

--- 2021/day04/solution.js
+++ 2021/day04/solution.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 let data = fs
-	.readFileSync('./2021/day04/control.txt', 'utf-8')
+	.readFileSync('./2021/day04/data.txt', 'utf-8')
 	.trim()
 	.split(/\n\n/)
 	.map((x, i) => {
@@ -13,6 +13,7 @@ let data = fs
 	});
 const bingoDraw = data.shift();
 const boards = [];
+//create a new board of lines (5) and columns (5) from board
 data.forEach((board) => {
 	// console.log(board);
 	const tempBoard = [];
@@ -29,30 +30,33 @@ data.forEach((board) => {
 
 	boards.push(tempBoard);
 });
+const emptyBoard = new Array(10).fill([ -1, -1, -1, -1, -1 ]);
 
 // console.log(boards);
 const solution = (boards, bingoDraw, p = 1) => {
 	const draw = bingoDraw.slice(0, 5);
-	// console.log(draw);
-	let won = false;
 	const part2 = [];
 
 	for (let i = 5; i < bingoDraw.length; i++) {
 		//i is bingo draw #
 		const currentDraw = bingoDraw[i];
 		draw.push(currentDraw);
+		// console.log(draw.length);
+		let won = false;
 
 		for (let j = 0; j < boards.length; j++) {
 			//j is board#
 			const board = boards[j];
 			let win = false;
-			let winLine;
+			let winLine; // unused
 			for (let k = 0; k < board.length; k++) {
 				const line = board[k];
 				if (line.every((x) => draw.some((y) => y === x))) {
 					win = true;
 					winLine = line;
-					if (p === 1) break;
+					boards[j] = emptyBoard; // marked this board as won and never be part of draw anymore
+					break;
+					// if (p === 1) break;
 				}
 			}
 			if (win) {
@@ -64,17 +68,10 @@ const solution = (boards, bingoDraw, p = 1) => {
 					.reduce((a, c) => a + c, 0);
 				if (p === 1) return unMarkedNum * currentDraw;
 
-				console.log(currentDraw);
 				part2.push(unMarkedNum * currentDraw);
-				// console.log(currentDraw, draw, unMarkedNum);
-
-				// break;
+				// console.log({ i, draw, currentDraw, j, winLine }); **uncomment this line to virtually see the draws
 			}
 		}
-		// return part2;
-		// if (won) {
-		// 	break;
-		// }
 	}
 	return part2;
 };
