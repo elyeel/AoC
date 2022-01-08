13db83034323027f470c8c46655d4188c639dba0
Risyardi Iswanto <elyeel@yahoo.com>
2022-01-08 00:01:47 -0800

    solved 2016 day07

--- 2016/day07/solution.js
+++ 2016/day07/solution.js
@@ -3,15 +3,27 @@ import fs from 'fs';
 const lines = fs.readFileSync('./2016/day07/data.txt', 'utf-8').split(/\n/);
 
 // console.log(lines.length);
-const check = (str) => {
-	const regex = /\[?(\w)(\w)\2\1\]?/;
+const check = (str, regex) => {
 	// console.log(typeof str);
 	if (str.match(regex)) {
 		const [ s, c1, c2 ] = str.match(regex);
 		if (c1 === c2) return false;
-		if (!regex.test(str)) return false;
-		else return true;
+		return regex.test(str);
+		// if (!regex.test(str)) return false;
+		// else return true;
 	} else return false;
+};
+
+const regexABBA = /\[?(\w)(\w)\2\1\]?/;
+const regexABA = /\[?(\w)(\w)\1\]?/;
+
+const checkABA = (str) => {
+	const abaArr = [];
+	for (let i = 0; i < str.length - 2; i++) {
+		const element = str.slice(i, i + 3);
+		if (regexABA.test(element)) abaArr.push(element);
+	}
+	return abaArr;
 };
 
 // console.log(
@@ -22,16 +34,34 @@ const check = (str) => {
 // console.log(check('ajtqcsfdarjrddrzo'));
 // console.log(check('[nzojrnbfrxvappppu]'));
 
-const solution = (lines) => {
-	const regex = /(\w)(\w)\2\1/g;
-	const result = lines.map((line) => {
-		const hypernet = line.match(/\[\w+\]/g);
-		const nonHypernet = line.split(/\[\w+\]/);
-		// console.log({ hypernet, nonHypernet });
-		if (hypernet.some((str) => check(str.toString()))) return false;
-		if (nonHypernet.some((str) => check(str))) return true;
-	});
-	console.log(result.filter((x) => x === true).length);
-	return result;
+checkABA('nniczueulxtdsmkniex');
+
+const solution = (lines, part = 1) => {
+	if (part === 1) {
+		const result = lines.map((line) => {
+			const hypernet = line.match(/\[\w+\]/g);
+			const nonHypernet = line.split(/\[\w+\]/);
+			// console.log({ hypernet, nonHypernet });
+			if (hypernet.some((str) => check(str, regexABBA))) return false;
+			if (nonHypernet.some((str) => check(str, regexABBA))) return true;
+		});
+		console.log(result.filter((x) => x === true).length);
+		return result;
+	} else {
+		const result = lines.map((line) => {
+			const res = [];
+			const hypernet = line.match(/\[\w+\]/g).map((x) => checkABA(x)).flat();
+			const nonHypernet = line.split(/\[\w+\]/).map((x) => checkABA(x)).flat();
+			// console.log({ hypernet, nonHypernet });
+			hypernet.forEach((elem) => {
+				const test = nonHypernet.some(
+					(x) => x[1] === elem[0] && x[0] === elem[1]
+				);
+				if (test) res.push(test);
+			});
+			return res;
+		});
+		console.log(result.filter((x) => x[0] === true).length);
+	}
 };
-solution(lines);
+solution(lines, 2);
