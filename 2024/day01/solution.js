19ee5a55fa2759e68d3379bfc6013731838caee4
Risyardi Iswanto <elyeel@yahoo.com>
2025-01-05 13:38:07 -0800

    Solved 2024/day01 part2

--- 2024/day01/solution.js
+++ 2024/day01/solution.js
@@ -9,16 +9,35 @@ const inputs = fs
 console.log("Data length : ", inputs.length, "- data line #8 : ", inputs[7]);
 
 const solution = (data) => {
+  // mapping of each number occurance
+  const rightOccurances = {};
+  data.forEach((element, i) => {
+    // console.log(element[1], rightOccurances[element[1]]);
+    rightOccurances[element[1]]
+      ? rightOccurances[element[1]]++
+      : (rightOccurances[element[1]] = 1);
+  });
+
   // sort data [x, y]
   const leftData = data.map((pair) => pair[0]).sort((a, b) => a - b);
   const rightData = data.map((pair) => pair[1]).sort((a, b) => a - b);
-  // console.log(leftData);
 
-  const result = data
+  // console.log(leftData);
+  // console.log("=> ", rightOccurances[3]);
+
+  // result1 is part 1 solution
+  const result1 = data
     .map((pair, i) => Math.abs(rightData[i] - leftData[i]))
     .reduce((a, c) => a + c);
 
-  return result;
+  // result2 is part 2 solution
+  const result2 = data
+    .map((pair) =>
+      rightOccurances[pair[0]] ? pair[0] * rightOccurances[pair[0]] : 0
+    )
+    .reduce((a, c) => a + c);
+
+  return { part1: result1, part2: result2 };
 };
 
 console.log(solution(inputs));
