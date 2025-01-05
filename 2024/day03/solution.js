d3b7ec62ae6744a73a5404eb1a2c7159f5652d3d
Risyardi Iswanto <elyeel@yahoo.com>
2025-01-04 21:51:45 -0800

    Solved part 2 of 2024/Day03

--- 2024/day03/solution.js
+++ 2024/day03/solution.js
@@ -1,7 +1,11 @@
 import fs from "fs";
-const inputs = fs.readFileSync("./2024/day03/data.txt", "utf8").split("\n");
+const startTime = Date.now();
 
-console.log(inputs.length);
+// inputs -> splitted per line; input -> oneliner string
+const inputs = fs.readFileSync("./2024/day03/data.txt", "utf8").split("\n");
+const input = fs.readFileSync("./2024/day03/data.txt", "utf-8").trim();
+
+console.log(inputs.length, input.length);
 
 const regex = /mul\((\d+),(\d+)\)/g;
 
@@ -13,9 +17,61 @@ const solution = (lines) => {
   return result;
 };
 
-console.log(solution(inputs));
+console.log("part 1 : ", solution(inputs));
 
 // splitting the dos and don'ts then merge them after removing anything after don'ts
 const splitterMerger = (data) => {
-  const splitDo = data.split("do()");
+  // console.log(data);
+  // split do()s
+  const splitDo = data.map((x) => x.split("do()"));
+  // console.log(splitDo);
+  // split Don't()
+  const splitDont = splitDo.map((x) => {
+    // console.log("splitDo : ", splitDo);
+    const resDont = x.map((y) => y.split("don't()")[0]);
+    return resDont;
+  });
+  return splitDont;
 };
+
+// console.log("Result = ", splitterMerger(inputs));
+
+// do solution for part2 -> the result is too high!
+const part2 = (data) =>
+  splitterMerger(data).map((x) => {
+    let result = 0;
+    x.map((y) => {
+      // console.log("here ", y); -> already a string
+      [...y.matchAll(regex)].forEach((mul) => (result += mul[1] * mul[2]));
+    });
+    return result;
+  }); //solution(y))); //y.map((z) => solution(z))));
+
+console.log(
+  "part 2 : ",
+  part2(inputs).reduce((a, c) => a + c, 0)
+);
+
+// a redo of the solution assuming input is a oneliner
+const solutionB = (data) => {
+  console.log(data.length);
+  let result = 0;
+  [...data.matchAll(regex)].forEach((mul) => (result += mul[1] * mul[2]));
+  return result;
+};
+
+// removing all don't section from the string by:
+// adding 'do()' at the beginning then split the string by "don't()"
+// then split again by "do()"
+// remove the first element from the result before
+// join result into a string again
+
+const conditionalString = ("do()" + input)
+  .split("don't()")
+  .flatMap((e) => e.split("do()").slice(1))
+  .join();
+
+console.log("Part 1 : ", solutionB(input));
+console.log("Part 2 : ", solutionB(conditionalString));
+
+console.log("solved in " + (Date.now() - startTime) + " ms.");
